1d195d20ca02f46db7992680c0d30936c8baf371
MrMendonez <dmendonez@gmail.com>
2016-01-20 15:40:23 -0500

    reviewed double & triple equals. included a summary.

--- tutorial5-understanding-references-to-objects.js
+++ tutorial5-understanding-references-to-objects.js
@@ -10,3 +10,7 @@ console.log(Bucky.favFood);
 
 // Summary: When assigning a variable to a JavaScript object and then changing the values of that variable you are also changing the values of the JavaScript object since both the JavaScript object and the variable point to the same reference.
 
+console.log(19 == "19"); // true
+console.log(19 === "19"); // false
+
+// Summary: Double equals compares the value only. On line 13, 19 is an integer and "19" is a string but their values are the same. Double equals is only comparing the values not the type therefore it outputs true. Triple equals compares both the value and the type. So on line 14, even though the values are the same the type is not. 19 is an integer and "19" is a string therefore it outputs false.
