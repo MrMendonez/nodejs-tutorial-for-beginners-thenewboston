3f442ab3dafac559df0e4e4a621c8600a056fe82
MrMendonez <dmendonez@gmail.com>
2016-01-20 15:33:24 -0500

    added summary to tutorial5

--- tutorial5-understanding-references-to-objects.js
+++ tutorial5-understanding-references-to-objects.js
@@ -7,3 +7,6 @@ var Person = Bucky;
 
 Person.favFood = "salad";
 console.log(Bucky.favFood);
+
+// Summary: When assigning a variable to a JavaScript object and then changing the values of that variable you are also changing the values of the JavaScript object since both the JavaScript object and the variable point to the same reference.
+
